8904b35bef177787a8d370400b21af1622e684cc
chwilluweit <willuweit@ant.uni-bremen.de>
2020-05-04 16:26:26 +0200

    Aufgaben abgeaendert. Finale Version

--- Exercise_1/solution_tester/functions.py
+++ Exercise_1/solution_tester/functions.py
@@ -6,37 +6,41 @@ def check_index_vector(variable_name, k, lenk, startv, endv):
     if len(k) != lenk:
         print('Incorrect length for ' + variable_name + '.')
         return
-    if k[0] != startv:
+    elif k[0] != startv:
         print('Vector ' + variable_name + ' does not start with ' + str(startv) + '.')
         return
-    if k[-1] != endv:
+    elif k[-1] != endv:
         print('Vector ' + variable_name + ' does not end with ' + str(endv) + '.')
         return
+    else:
+        print('Index vector correct')
 
 def check_signal_vector(variable_name, x, lenx):
     if len(x) != lenx:
         print('Incorrect length for ' + variable_name + '.')
         return
+    else:
+        print('Correct length for ' + variable_name + '.')
         
 def check_variable(variable_name, value_cor, value_in):
     if value_cor !=value_in:
         print(variable_name + ' is not equal to ' + str(value_cor))
-
+    else:
+        print('Correct length for ' + variable_name + '.')
 
 
 
     
-def exercise1(Omega0, k, xsin, Omega0b, kb, xsinb):
-   check_variable('Omega0', 2*np.pi* 400/8000, Omega0)
-   check_index_vector('k', k, 60, 0, 59)
-   check_signal_vector('xsin', xsin, 60)
-   check_variable('Omega0b', 2*np.pi* 960/8000, Omega0b)
-   check_index_vector('kb', kb, 0, 59)
-   check_signal_vector('xsinb', xsinb, 60)
+def exercise1(k, x_a, x_b, Omega_a, Omega_b):
+    check_index_vector('k', k, 60, 0, 59)
+    check_variable('Omega_a', 2*np.pi* 400/8000, Omega_a)
+    check_signal_vector('x_a', x_a, 60)
+    check_variable('Omega_b', 2*np.pi* 960/8000, Omega_b)
+    check_signal_vector('x_b', x_b, 60)
 
 def exercise2(k, x):
-   check_index_vector('k', k, 16, -5, 10)
-   check_signal_vector('x', x, 16)
+    check_index_vector('k', k, 16, -5, 10)
+    check_signal_vector('x', x, 16)
 
 def exercise3(k, x, kc4, xc4, kc8, xc8):
     check_index_vector('k', k, 16, 0, 15)
